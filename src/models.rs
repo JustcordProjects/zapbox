717a992584d2c8c7552532838d78a893c9ec7238
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-05-31 18:45:03 +0200

    fix: add pub to all fields

--- src/models.rs
+++ src/models.rs
@@ -8,20 +8,20 @@ pub enum MessageKind {
 
 #[derive(Debug, Serialize)]
 pub struct Message {
-    kind: MessageKind,
-    content: String,
+    pub kind: MessageKind,
+    pub content: String,
 }
 
 #[derive(Debug, Serialize)]
 pub struct ExecResult {
-    messages: Vec<Message>,
-    exitcode: i32,
+    pub messages: Vec<Message>,
+    pub exitcode: i32,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Input {
-    src: String,
-    stdin: Option<String>,
+    pub src: String,
+    pub stdin: Option<String>,
 }
 
 #[derive(Debug, Serialize)]
@@ -33,7 +33,7 @@ pub enum Status {
 
 #[derive(Debug, Serialize)]
 pub struct Output {
-    status:   Status,
-    compiler: ExecResult,
-    runtime:  Option<ExecResult>,
+    pub status:   Status,
+    pub compiler: ExecResult,
+    pub runtime:  Option<ExecResult>,
 }

29baa075d92764b3029c85ce9c1af6d83a4242c0
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-01 10:03:01 +0200

    feat: write input data to temp files

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,31 @@ mod models;
 use models::*;
 
 use std::env;
+use std::fs::File;
 use std::process as proc;
 
-fn main() {
+use std::io::Write;
+
+fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("usage: zapc-wrapper <json-data...>");
         proc::exit(1);
     }
 
-    // TODO: better error handling
-    let input: Input = serde_json::from_str(&args[1]).expect("invalid input json");
-    dbg!(input);
+    let input: Input = serde_json::from_str(&args[1])?;
+    dbg!(&input);
+
+    let dir = tempfile::tempdir()?;
+    let dir = dir.path();
+
+    let mut src_file = File::create(dir.join("src.zp"))?;
+    write!(src_file, "{}", &input.src)?;
+
+    if let Some(stdin) = &input.stdin { 
+        let mut stdin_file = File::create(dir.join("stdin.txt"))?;
+        write!(stdin_file, "{}", stdin)?;
+    }
+
+    Ok(())
 }

4444a48eba44cf15a30f88b61b705801e2db126f
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-01 12:00:56 +0200

    feat: add subcommands and implement building container image

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,42 @@ use models::*;
 
 use std::env;
 use std::fs::File;
-use std::process as proc;
+
+use std::process::{
+    self as proc,
+    Command,
+};
 
 use std::io::Write;
 
-fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("usage: zapbox <json-data...>");
+const IMAGE_NAME: &str = "zapbox-image";
+
+fn usage() {
+    // did you know that println![] or println!{} is valid in rust?
+    // most useless facts #0
+    eprintln!{r#"
+usage: zapbox <command> [args...]
+commands:
+    zapbox run <json-data...>    - compile and run given source code (outputs json to stdout)
+    zapbox build                 - build the podman image (with name {IMAGE_NAME})
+"#};
+}
+
+fn build_image() -> anyhow::Result<()> {
+    let status = Command::new("podman")
+        .args(["build", "-t", IMAGE_NAME, "."])
+        .status()?;
+
+    if !status.success() {
+        eprintln!("failed to build the container image");
         proc::exit(1);
     }
 
-    let input: Input = serde_json::from_str(&args[1])?;
+    Ok(())
+}
+
+fn run(arg: &str) -> anyhow::Result<()> {
+    let input: Input = serde_json::from_str(arg)?;
     dbg!(&input);
 
     let dir = tempfile::tempdir()?;
@@ -29,4 +53,23 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 || args.len() > 3 {
+        eprintln!("unexpected amount of arguments");
+        usage();
+        proc::exit(1);
+    }
+
+    match args[1].as_str() {
+        "build" => build_image(),
+        "run"   => run(&args[2]),
+        _ => {
+            eprintln!("unknown command: {}", args[1]);
+            usage();
+            Ok(())
+        }
+    }
 }

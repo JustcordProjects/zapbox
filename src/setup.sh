009c45a22d9a8e5c16c8888be975cb380b2c7c67
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-02 16:04:02 +0200

    fix: gpg keys issues

--- src/setup.sh
+++ src/setup.sh
@@ -7,7 +7,13 @@ export LC_ALL=C
 ZAP_VERSION=0.2.0
 ZAP_URL="https://github.com/thezaplang/zap/releases/download/v$ZAP_VERSION/zap-$ZAP_VERSION-linux-x86_64.tar.gz"
 
-#grep -q '^staff:' /etc/group || echo 'staff:x:50:' >> /etc/group
+mkdir -p /etc/apt/apt.conf.d
+
+cat > /etc/apt/apt.conf.d/99insecure <<'EOF'
+Acquire::AllowInsecureRepositories "true";
+Acquire::AllowDowngradeToInsecureRepositories "true";
+APT::Get::AllowUnauthenticated "true";
+EOF
 
 apt-get update
 apt-get install -y --no-install-recommends \
